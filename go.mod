18f1a16f7daa7f5d0430154cda2ff76e53d644d1
Bence Szabo <beszabo@protonmail.com>
2023-09-17 18:07:09 +0200

    Implemented command line usage

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/packages
+
+go 1.21.1
+
+require github.com/gorilla/mux v1.8.0
---
+++ simpleHTTPServer.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"flag"
+	"os"
+	"path/filepath"
+	"strconv"
+	"bufio"
+)
+
+type Arguments struct {
+	port string
+	rootDir string
+	transfer bool
+	ssl bool
+	crt string
+	key string
+	uploadPassword string
+	getPassword string
+}
+
+func args() Arguments {
+	f := Arguments{}
+	flag.StringVar(&f.port, "p", "9000", "Port to listen on")
+	flag.StringVar(&f.rootDir, "t", ".", "Root directory to serve files from")
+	flag.BoolVar(&f.transfer, "u", false, "Accept transfer files from clients")
+	flag.BoolVar(&f.ssl, "s", false, "Use SSL")
+	flag.StringVar(&f.crt, "c", "", "SSL certificate file (.crt)")
+	flag.StringVar(&f.key, "k", "", "SSL key file (.key)")
+	flag.StringVar(&f.uploadPassword, "upw", "", "Password for uploading files")
+	flag.StringVar(&f.getPassword, "pw", "", "Password for downloading files")
+	flag.Parse()
+	return f
+}
+
+func listAllFiles(rootDir string) []string {
+	var files []string
+	filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
+		// remove rootDir from path
+		path = path[len(rootDir):]
+		files = append(files, path)
+		return nil
+	})
+	return files
+}
+
+
+
+func main() {
+	arguments := args()
+
+	fmt.Printf("Serving files from: %s\n", arguments.rootDir)
+	fmt.Printf("Listening on port: %s\n", arguments.port)
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Download the file on: /id/<id_of_the_file>\n")
+		allFiles := listAllFiles(arguments.rootDir)
+		for id, file := range allFiles {
+			fmt.Fprintf(w, "%d\t%s\n", id, file)
+		}
+	})
+
+	r.HandleFunc("/id/{id}", func (w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id := vars["id"]
+	
+		allFiles := listAllFiles(arguments.rootDir)
+		
+		idInt, _ := strconv.Atoi(id)
+
+		if idInt < len(allFiles) {
+			relativeFilePath := allFiles[idInt]
+			absoluteFilePath := arguments.rootDir + relativeFilePath
+
+			// read file bytes
+			file, err := os.Open(absoluteFilePath)
+			if err != nil {
+				http.Error(w, "File could not be read", http.StatusNotFound)
+			}
+
+			// write file bytes to response
+			fileReader := bufio.NewReader(file)
+			fileReader.WriteTo(w)
+			file.Close()
+
+		} else {
+			// return with a bad request
+			http.Error(w, "Invalid ID", http.StatusBadRequest)
+		}
+	})
+
+	port := ":" + arguments.port
+
+	http.ListenAndServe(port, r)
+}
